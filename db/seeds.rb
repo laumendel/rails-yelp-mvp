45b7804945d8eb9ef279b8f2e463a33c72f04f73
Laura Mendelzon <laura.mendelzon@gmail.com>
2025-02-26 15:06:38 +0100

    whatevs

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,26 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+Restaurant.create(name: "Mister Poulet", address: "16 villa gaudelet", category: "belgian")
+puts "Created Mister Poulet"
+
+Restaurant.create(name: "Chez Yong", address: "16 villa gaudelet", category: "chinese")
+puts "Chez Yong"
+
+Restaurant.create(name: "Didoune", address: "16 villa gaudelet", category: "french")
+puts "Created Didoune"
+
+Restaurant.create(name: "Nobinobi", address: "16 villa gaudelet", category: "japanese")
+puts "Created Nobinobi"
+
+Restaurant.create(name: "Piacere", address: "16 villa gaudelet", category: "italian")
+puts "Piacere"
+
+puts "Finished! Created #{Restaurant.count} restaurants."
+
